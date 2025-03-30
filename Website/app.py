b78efd42e36e72f58bef67185216f7e359fc9c99
FranGoncal <franciscogoncalvesep@gmail.com>
2025-03-30 11:11:33 +0100

    Alteração para modo Debug

--- Website/app.py
+++ Website/app.py
@@ -31,5 +31,5 @@ def sobre():
     return render_template("sobre.html")
 
 if __name__ == "__main__":
-    #app.run(debug=True)
-    app.run(host="0.0.0.0", port=5000)
+    app.run(debug=True)
+    #app.run(host="0.0.0.0", port=5000)
